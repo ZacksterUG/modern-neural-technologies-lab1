0bffc59ce1611e4561e4bd57171da8f5fafdad71
ZacksterUG <fomkalex@mail.ru>
2025-04-01 16:10:57 +0500

    Добавлена поддержка многослойного перцептрона

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import argparse
 WEIGHTS_FILE = "model_weights.npz"
 
 data = np.load(WEIGHTS_FILE)
-W1 = data['W1']
-b1 = data['b1']
-W2 = data['W2']
-b2 = data['b2']
+layers = data['arr_0']
+W = []
+b = []
+
+for i in range(0, layers):
+    _w = data[f"arr_{i + 1}"]
+    _b = data[f"arr_{layers + i + 1}"]
+
+    W.append(_w)
+    b.append(_b)
 
 def predict(X: np.array, probabilities=True):
-    h1 = X @ W1 + b1
-    a1 = np.maximum(0, h1)
-    h2 = a1 @ W2 + b2
-    a2 = np.exp(h2) / np.sum(np.exp(h2), axis=1, keepdims=True)
+    for i in range(0, layers):
+        X = X @ W[i] + b[i]
+    a2 = np.exp(X) / np.sum(np.exp(X), axis=1, keepdims=True)
 
     if probabilities:
         return a2
@@ -57,7 +62,7 @@ if __name__ == "__main__":
                 print(f"{i}: {prob:.4f}")
         else:
             prediction = predict(processed_img, probabilities=False)
-            print(f"Predicted digit: {prediction[0]}")
+            print(f"Predicted digit: {prediction}")
             
     except Exception as e:
         print(f"Error: {str(e)}")
